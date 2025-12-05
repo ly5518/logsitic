aca0970399bb72e317ff31d5078e1f2fcca83e52
ly5518 <3357298452@qq.com>
2025-12-05 15:39:59 +0800

    Update logistic.py

--- logistic.py
+++ logistic.py
@@ -2,7 +2,6 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 
 
-# filename=r"c:\Users\E507\Documents\GitHub\logsitic\horseColicTraining.txt"
 train_dir = r"C:\Users\E507\Documents\GitHub\logsitic\horseColicTraining.txt"  
 test_dir  = r"C:\Users\E507\Documents\GitHub\logsitic\horseColicTest.txt"       
 #=====================
@@ -39,20 +38,20 @@ def main():
     X_train, y_train = load_dataset(train_dir)
     X_test,  y_test  = load_dataset(test_dir)
     print("使用梯度下降训练Logistic回归")
-    w_gd=gradient_descent_logistic(x,y,lr=0.001,n_iters=8000)
-    print(f"\n[GD]Learned w={w_gd}")
+    # w_gd=gradient_descent_logistic(x,y,lr=0.001,n_iters=8000)
+    # print(f"\n[GD]Learned w={w_gd}")
     print("\n使用sklearn LogisticRegression训练")
     clf=LogisticRegression(solver='lbfgs',max_iter=3000)
     clf.fit(X_train,y_train)
     print(f"[sklearn coef_={clf.coef_},intercept_={clf.intercept_}]")
-    y_pred_gd=(sigmoid(np.c_[np.ones((x.shape[0],1)),x]@w_gd)>=0.5).astype(int)
-    acc_gd=np.mean(y_pred_gd==y)
+    # y_pred_gd=(sigmoid(np.c_[np.ones((x.shape[0],1)),x]@w_gd)>=0.5).astype(int)
+    # acc_gd=np.mean(y_pred_gd==y)
     y_pred_sk=clf.predict(X_test)
     acc_sk=np.mean(y_pred_sk==y_test)
     print(f"\n训练集准确率:")
-    print(f"自写GD Logistic 回归 accuracy={acc_gd:.4f}")
+    # print(f"自写GD Logistic 回归 accuracy={acc_gd:.4f}")
     print(f"sklearn LogisticRegression accuracy={acc_sk:.4f}")
-    plot_compare_boundaries(x,y,w_gd,clf)
+    # plot_compare_boundaries(x,y,w_gd,clf)
 # 读取测试集
 
 
